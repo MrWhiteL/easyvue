2798ebac0d300e8492adeef0a14da883972f633c
MrWhiteL <840191327@qq.com>
2018-07-26 18:55:35 +0800

    框架基础构建

---
+++ src/utils/sys-config.js
@@ -0,0 +1,3 @@
+export const host='192.168.0.19';
+export const port=8280
+export const server_context='/easyvue';
---
+++ src/views/exception/500.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "500"
+    }
+</script>
+
+<style scoped>
+
+</style>
